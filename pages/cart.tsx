103a8c5d047b1c40ae2a1cc4eed2d961db338e42
unknown <dashing.hank@gmail.com>
2022-03-09 16:51:49 +0800

    0451

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,14 +1,32 @@
+import CryptoJS from "crypto-js";
+import urlencode from "urlencode";
+
 import type { NextPage } from "next";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { parse } from "querystring";
+import { parse, ParsedUrlQuery } from "querystring";
 const Cart: NextPage = (props: any) => {
   const [result, setResult] = useState("");
   const router = useRouter();
   if (router.query.cvs == "1") {
     return <div dangerouslySetInnerHTML={MyComponent(props.result.htm.toString())} />;
   } else if (router.query.cvs == "2") {
-    console.log(props.result);
+    let res = props.result;
+    console.log("props.result:", props.result);
+    console.log("props.result slice:", props.result.slice(0, 20));
+
+    // let lockedData = "uvI4yrErM37XNQkXGAgRgJAgHn2t72jahaMZzYhWL1HmvH4WV18VJDP2i9pTbC+tby5nxVExLLFyAkbjbS2Dvg==";
+
+    // let key = CryptoJS.enc.Utf8.parse("ejCk326UnaZWKisg"); // key：必須16個字元
+    // let iv = CryptoJS.enc.Utf8.parse("q9jcZX8Ib9LM8wYk");
+    // var bytes = CryptoJS.AES.decrypt(lockedData, key, {
+    //   iv: iv,
+    //   mode: CryptoJS.mode.CBC,
+    //   padding: CryptoJS.pad.Pkcs7,
+    // });
+    // console.log(bytes.toString(CryptoJS.enc.Utf8));
+    // let data = urlencode.decode(bytes.toString(CryptoJS.enc.Utf8));
+    // console.log(data);
   }
 
   return (
@@ -42,7 +60,8 @@ export async function getServerSideProps(context) {
       });
 
       context.req.on("end", () => {
-        const postData = parse(postBody);
+        const postData: ParsedUrlQuery = parse(postBody);
+
         resolve(postData);
       });
     });
