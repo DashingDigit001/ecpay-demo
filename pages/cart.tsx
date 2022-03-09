e0a96b31d9611cb9ca84ebc4ac50139056cfe814
unknown <dashing.hank@gmail.com>
2022-03-09 17:27:28 +0800

    0527

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -13,22 +13,21 @@ const Cart: NextPage = (props: any) => {
   } else if (router.query.cvs == "2") {
     let res = props.result;
     console.log("props.result type:", typeof props.result);
-    console.log("props.result type:", props.ResultData);
-    console.log("props.result type:", JSON.stringify(props.result.ResultData));
-    // console.log("props.result:", props.result.toString());
+    console.log("props.result type:", props.result.ResultData);
+    let resultData = JSON.parse(props.result.ResultData);
+    let lockedData = resultData.Data;
+    console.log("data", lockedData);
 
-    // let lockedData = "uvI4yrErM37XNQkXGAgRgJAgHn2t72jahaMZzYhWL1HmvH4WV18VJDP2i9pTbC+tby5nxVExLLFyAkbjbS2Dvg==";
-
-    // let key = CryptoJS.enc.Utf8.parse("ejCk326UnaZWKisg"); // key：必須16個字元
-    // let iv = CryptoJS.enc.Utf8.parse("q9jcZX8Ib9LM8wYk");
-    // var bytes = CryptoJS.AES.decrypt(lockedData, key, {
-    //   iv: iv,
-    //   mode: CryptoJS.mode.CBC,
-    //   padding: CryptoJS.pad.Pkcs7,
-    // });
-    // console.log(bytes.toString(CryptoJS.enc.Utf8));
-    // let data = urlencode.decode(bytes.toString(CryptoJS.enc.Utf8));
-    // console.log(data);
+    let key = CryptoJS.enc.Utf8.parse("XBERn1YOvpM9nfZc"); // key：必須16個字元
+    let iv = CryptoJS.enc.Utf8.parse("h1ONHk4P4yqbl5LK");
+    var bytes = CryptoJS.AES.decrypt(lockedData, key, {
+      iv: iv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7,
+    });
+    console.log(bytes.toString(CryptoJS.enc.Utf8));
+    let data = urlencode.decode(bytes.toString(CryptoJS.enc.Utf8));
+    console.log(data);
   }
 
   return (
