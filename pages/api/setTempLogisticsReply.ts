6490d8f44c8136f7a7ed867dbb571d3f2d36f01c
unknown <dashing.hank@gmail.com>
2022-03-09 14:53:36 +0800

    console.log

--- pages/api/setTempLogisticsReply.ts
+++ pages/api/setTempLogisticsReply.ts
@@ -2,6 +2,7 @@ import ecpay_payment from "ecpay_aio_nodejs/lib/ecpay_payment.js";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req, res: NextApiResponse) {
+  console.log("hello temp");
   console.log("========================================================================================");
   console.log(req);
   console.log("========================================================================================");
